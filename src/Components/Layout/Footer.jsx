d248a19b69052eefbae8ba503ad1c3cca6a6737a
Kevin Havn <kevin-havn@hotmail.com>
2024-05-24 18:25:52 +0200

    added sticky to footer

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 p-4 text-white mt-auto">
+    <footer className="bg-gray-800 p-4 sticky bottom-0 text-white mt-auto">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 My Application</p>
       </div>
