8267037bf5974fe0e0f1c46f0a0ddd65a3b637f1
Kevin Havn <kevin-havn@hotmail.com>
2024-05-24 18:26:11 +0200

    added venues page

---
+++ src/Pages/Venues.jsx
@@ -0,0 +1,13 @@
+import VenuesCard from "../Components/VenuesCard";
+
+const Venues = () => {
+	return (
+		<div className="min-h-screen bg-gray-100">
+			<main className="p-4">
+				<VenuesCard />
+			</main>
+		</div>
+	);
+};
+
+export default Venues;
