6657e8721ecb9dbc725ec4b7fb48a4e2f5c36345
Kevin Havn <kevin-havn@hotmail.com>
2024-05-24 18:25:11 +0200

    added venues page to routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout/Index";
-import Home from './Pages/Home';
-import About from './Pages/About';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Venues from "./Pages/Venues";
 
 const App = () => {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/venues" element={<Venues />} />
+			</Routes>
+		</Layout>
+	);
 };
 
 export default App;
