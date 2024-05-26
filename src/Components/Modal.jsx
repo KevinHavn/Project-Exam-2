b9b5d1cc528d35ddd478f7110212f40245c0c439
Kevin Havn <kevin-havn@hotmail.com>
2024-05-26 17:55:41 +0200

    Added support for clicking outside of the modal to close it

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
 const Modal = ({ onClose, children }) => {
 	return (
-		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-			<div className="bg-white p-4 rounded-md shadow-md relative">
+		<div
+			className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+			onClick={onClose} // Close the modal when clicking outside the content
+		>
+			<div
+				className="bg-white p-4 rounded-md shadow-md relative"
+				onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the content
+			>
 				<button
 					onClick={onClose}
 					className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
